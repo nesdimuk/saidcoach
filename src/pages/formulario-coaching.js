0282c3acada4bc3739d68d604a44165befdacd84
Marcelo Said <saidtrainer@192.168.1.10>
2025-06-23 08:09:25 -0400

    Agrega página de formulario de coaching personalizado

---
+++ src/pages/formulario-coaching.js
@@ -0,0 +1,18 @@
+export default function FormularioCoaching() {
+  return (
+    <div className="min-h-screen p-4 bg-white flex flex-col items-center">
+      <h1 className="text-3xl font-bold mb-6 text-center">Formulario Coaching Personalizado</h1>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfKlIvWaS8zUwi4TQYHfeiQwc8cdrJoaeNTknkAo6mNa5ZF8w/viewform?embedded=true"
+        width="100%"
+        height="1600"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        title="Formulario Coaching"
+      >
+        Cargando…
+      </iframe>
+    </div>
+  );
+}
